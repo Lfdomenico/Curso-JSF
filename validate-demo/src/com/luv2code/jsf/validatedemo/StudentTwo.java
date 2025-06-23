33261e28c78669c96b717ca3ca5c9a5eaebb82f2
Luiz Fernando Moreira Domênico <luizfmd21@gmail.com>
2025-06-22 23:25:22 -0300

    Projetos - Curso

---
+++ validate-demo/src/com/luv2code/jsf/validatedemo/StudentTwo.java
@@ -0,0 +1,48 @@
+package com.luv2code.jsf.validatedemo;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentTwo {
+	
+	private String firstName;
+	private String lastName;
+	private int freePasses;
+	private int postalCode;
+	
+	public StudentTwo() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+}
